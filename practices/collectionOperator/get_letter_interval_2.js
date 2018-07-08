4647c0b1a9b73127c3073d5a8dfd634efc08438e
BaleLin <544097676@qq.com>
2018-07-08 23:25:10 +0800

    update

--- practices/collectionOperator/get_letter_interval_2.js
+++ practices/collectionOperator/get_letter_interval_2.js
@@ -2,21 +2,46 @@
 
 function get_letter_interval_2(number_a, number_b) {
   let temp = [];
+  let count = 0;
   if(number_a<number_b){
-    for(let num=number_a;num<=number_b;num++){
-      var s = ''
-      while (num > 0) {
+    for(let i=number_a;i<=number_b;i++){
+    let num = i;
+     var s = '';
+       while (num > 0) {
         var m = num % 26
         if (m === 0) m = 26
         s = (m + 9).toString(36) + s
         num = (num - m) / 26
       }
       temp.push(s);
+      }
+      return temp;
     }
-  }
-  console.log(temp);
-  return temp;
+    else if(number_a>number_b){
+      for(let i=number_b;i<=number_a;i++){
+        let num = i;
+         var s = '';
+           while (num > 0) {
+            var m = num % 26
+            if (m === 0) m = 26
+            s = (m + 9).toString(36) + s
+            num = (num - m) / 26
+          }
+          temp.push(s);
+          }
+          return temp.reverse();
+    }else{
+      let num = number_a;
+      var s = '';
+        while (num > 0) {
+         var m = num % 26
+         if (m === 0) m = 26
+         s = (m + 9).toString(36) + s
+         num = (num - m) / 26
+       }
+       temp.push(s);
+       return temp;
+       }
 }
-
 module.exports = get_letter_interval_2;
 
